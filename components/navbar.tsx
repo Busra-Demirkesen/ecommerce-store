af7947ea3d1d60e20f81e68265c8fcacd65591c0
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-07-31 15:22:28 +0200

    chore: update dependencies and add Tailwind CSS configuration
    - Added clsx and tailwind-merge dependencies.
    - Updated next, react, and react-dom versions.
    - Added postcss and autoprefixer for Tailwind CSS support.
    - Created postcss.config.js for PostCSS configuration.
    - Created tailwind.config.js for Tailwind CSS configuration.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import Container from "./ui/container";
+import MainNav from "./main-nav";
+
 const Navbar = () =>{
     return(
         <>
-        <div className="border-b">
-            <p>Navbar</p>
+        <div className="border-b ">
+            <Container>
+                <div className="realative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                <Link href="/" className="ml-4 lg:ml-0 gap-x-2">
+                    <p className="font-bold text-xl text-end">STORE</p>
+                </Link>
+                <MainNav data={[]} />
+                </div>
+            </Container>
         </div>
         </>
     )
