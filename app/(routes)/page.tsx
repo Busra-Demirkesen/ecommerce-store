ba5ad8051a1a45f36108b02930e334438a90bdc6
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-01 00:25:39 +0200

    refactor: improve formatting and spacing in HomePage component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,21 +8,22 @@ import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 
-const HomePage = async() => {
+const HomePage = async () => {
 
   const billboard = await getBillboards("efc8cdf5-ca62-42b9-857c-6f8baeb40740");
 
-  const products = await getProducts({isFeatured:true});
-    return (
-      <Container>
-        <div className="space-y-10 pb-10">
-            <Billboard data={billboard}/>
-        </div>
+  const products = await getProducts({ isFeatured: true });
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products}/>
+          <ProductList title="Featured Products" items={products} />
         </div>
-      </Container>
-      
-    )
+      </div>
+    </Container>
+
+  )
 }
 export default HomePage;
