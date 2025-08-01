a4c7f01f6fa9742db1d3bcedc84e2b5c39cf8359
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-01 23:43:06 +0200

    feat: implement category page with filtering; add actions for fetching categories, colors, and sizes

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () =>{
     return (
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
-                <p className="text-center text-xs text-black">© 2023 Your Company. All rights reserved.</p>
+                <p className="text-center text-xs text-black">© 2025 e-Commerce. All rights reserved.</p>
             </div>
         </footer>
     )
--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,45 +1,53 @@
 'use client';
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+
+import { Tab } from '@headlessui/react';
 import Image from 'next/image';
-import { Image as ImageType} from '@/types';
+import { Image as ImageType } from '@/types';
 import GalleryTab from './gallery-tab';
 
 interface GalleryProps {
   images: ImageType[];
 }
 
-const Gallery:React.FC<GalleryProps> = ({
-  images
-}) =>{
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
   return (
-    <div>
-      <TabGroup as='div' className="flex flex-col-reverse">
-        <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
-          <TabList className="grid grid-cols-4 gap-6">
-            {images.map((image) => (
-              <GalleryTab key={image.id} image={image}/>
-            ))}
-          </TabList>
-        </div>
-        <TabPanels className="aspect-square w-full">
-            {images.map((image) => (
-              <TabPanel key={image.id}>
-                  <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden'>
-                    <Image
-                        fill
-                        src={image.url}
-                        alt=''
-                        className='object-cover object-center'
-                    
-                    />
+    <div className="w-full">
 
-                  </div>
-              </TabPanel>
-            ))}
-        </TabPanels>
-      </TabGroup>
+      <Tab.Group as="div" className="flex flex-col lg:flex-row gap-y-6 lg:gap-x-8">
+  {/* LEFT: Thumbnails + Main Image */}
+  <div className="w-full lg:w-1/2">
+    {/* Main image panels */}
+    <Tab.Panels className="w-full mb-4">
+      {images.map((image) => (
+        <Tab.Panel key={image.id}>
+          <div className="aspect-square relative w-full max-w-2xl mx-auto overflow-hidden rounded-lg border border-gray-200 shadow-sm">
+            <Image
+              fill
+              src={image.url}
+              alt=""
+              className="object-cover object-center"
+            />
+          </div>
+        </Tab.Panel>
+      ))}
+    </Tab.Panels>
+
+    {/* Thumbnail list */}
+    <div className="mx-auto mt-2 hidden w-full max-w-2xl sm:block lg:max-w-none">
+      <Tab.List className="grid grid-cols-4 gap-4">
+        {images.map((image) => (
+          <GalleryTab key={image.id} image={image} />
+        ))}
+      </Tab.List>
     </div>
-  )
-}
+  </div>
+
+  {/* RIGHT: Info alanı */}
+  {/* <div className="w-full lg:w-1/2 px-4">İçerik buraya</div> */}
+</Tab.Group>
+
+    </div>
+  );
+};
 
 export default Gallery;
