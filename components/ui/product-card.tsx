11a3f58427b26337d5d37699bf5724d45ec61f6c
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-01 12:29:37 +0200

    feat: implement product page and currency formatting component; add getProduct action for fetching product details

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -3,16 +3,22 @@
 import { Product } from "@/types";
 import Image from "next/image";
 import IconButton from "@/components/ui/icon-button";
-import { Currency, Expand, ShoppingCart } from "lucide-react";
+import {  Expand, ShoppingCart } from "lucide-react";
 import { FC } from "react";
+import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
+  const handleClick = () =>{
+    router.push(`/product/${data?.id}`);
+  }
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4" onClick={handleClick}>
       <div className="relative aspect-square rounded-xl bg-gray-100">
         <Image
           alt="Product"
